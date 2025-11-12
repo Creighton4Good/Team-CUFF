be96710eb4a5a341e1894ff0cd8b921d5d642abe
Mal <malaynee@outlook.com>
2025-11-12 17:52:12 -0600

    initial commit

---
+++ spring/src/main/java/cuff/cuff_springboot/CuffSpringbootApplication.java
@@ -0,0 +1,13 @@
+package cuff.cuff_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuffSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuffSpringbootApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/cuff/cuff_springboot/CuffSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cuff.cuff_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuffSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

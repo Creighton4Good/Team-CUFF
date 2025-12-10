2ff993b0f1553d8079785a1c53ed95685e42baad
Erika Germinario <erika.germinario@gmail.com>
2025-12-10 10:40:59 -0600

    Fixed Spring notif error.

--- spring/src/main/java/cuff/cuff_springboot/repository/UserRepository.java
+++ spring/src/main/java/cuff/cuff_springboot/repository/UserRepository.java
@@ -2,8 +2,9 @@ package cuff.cuff_springboot.repository;
 import cuff.cuff_springboot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository <User, Integer> {
-    
+    List<User> findByNotificationsEnabledTrue();
 }
